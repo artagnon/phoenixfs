c63e673844e9f522b403879a8e9073c92bd82b22
Ramkumar Ramachandra <artagnon@gmail.com>
2011-03-02 11:08:15 +0530

    Don't allow gitfs to be run as `root`
    This restriction will save us from plugging several security holes in
    the long run.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include "gitfs.h"
+#include <unistd.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
+	if ((getuid() == 0) || (geteuid() == 0))
+		die("Running gitfs as root opens unnacceptable security holes");
+
 	return gitfs_fuse(argc, argv);
 }
