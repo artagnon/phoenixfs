ada7e01e0338727459e0ac6367c81d55c8d2db5f
Ramkumar Ramachandra <artagnon@gmail.com>
2011-03-03 09:21:58 +0530

    fuse: Don't pollute syslog
    Log to `/tmp/gitfs.log` instead.
    
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- fuse.c
+++ fuse.c
@@ -11,13 +11,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include <syslog.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/xattr.h>
 
 #if 1
-#define GITFS_DBG(f, ...) syslog(LOG_DEBUG, f, ## __VA_ARGS__)
+#define GITFS_DBG(f, ...) \
+{ \
+	FILE *logfh = fopen("/tmp/gitfs.log", "a");	\
+	if (logfh) { \
+		fprintf(logfh, "l. %4d: " f "\n", __LINE__, ##__VA_ARGS__); \
+		fclose(logfh); \
+	} \
+}
 #else
 #define GITFS_DBG(f, ...) while(0)
 #endif
