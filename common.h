6e8cda773f1db830e4697594e2b683510b55b824
Ramkumar Ramachandra <artagnon@gmail.com>
2011-03-17 22:03:16 +0530

    Move the GITFS_DBG macro to common.h
    Signed-off-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- common.h
+++ common.h
@@ -1,6 +1,19 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
+#if 1
+#define GITFS_DBG(f, ...) \
+{ \
+	FILE *logfh = fopen("/tmp/gitfs.log", "a");	\
+	if (logfh) { \
+		fprintf(logfh, "l. %4d: " f "\n", __LINE__, ##__VA_ARGS__); \
+		fclose(logfh); \
+	} \
+}
+#else
+#define GITFS_DBG(f, ...) while(0)
+#endif
+
 void die(const char *err, ...);
 
 #endif
--- fuse.h
+++ fuse.h
@@ -13,20 +13,6 @@
 #include "sha1.h"
 #include "buffer.h"
 #include "compress.h"
-
-#if 1
-#define GITFS_DBG(f, ...) \
-{ \
-	FILE *logfh = fopen("/tmp/gitfs.log", "a");	\
-	if (logfh) { \
-		fprintf(logfh, "l. %4d: " f "\n", __LINE__, ##__VA_ARGS__); \
-		fclose(logfh); \
-	} \
-}
-#else
-#define GITFS_DBG(f, ...) while(0)
-#endif
-
 struct env_t {
 	char *datapath;
 	char *mountpoint;
